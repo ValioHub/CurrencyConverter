58b21ea0a189632eb1db835d36e07cc9bc5d921a
Valentin <valentin.chivliev02@gmail.com>
2023-06-22 00:25:03 +0300

    Binding Values to Combo Boxes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,58 @@ namespace CurrencyConverter_Static
         public MainWindow()
         {
             InitializeComponent();
+            BindCurrency();
         }
 
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+            // Add rows in the DataTable'
+            dtCurrency.Rows.Add("Select", 0);
+            dtCurrency.Rows.Add("USD", 1);
+            dtCurrency.Rows.Add("EUR", 0.910247);
+            dtCurrency.Rows.Add("GBP", 0.783139);
+            dtCurrency.Rows.Add("INR", 81.938793);
+            dtCurrency.Rows.Add("AUD", 1.471434);
+            dtCurrency.Rows.Add("CAD", 1.316428);
+            dtCurrency.Rows.Add("SGD", 1.340416);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
+            // Create variable with double datatype to store converted value
+            double ConvertedValue;
 
+            // Check if the textbox is Null or Empty
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                // If is Null or Empty will show message
+                MessageBox.Show("Please enter currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Set focus on textbox
+                txtCurrency.Focus();
+                return;
+            }
+            // If currency is not selected or selected default text
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0) ;
+            {
+                // Show message
+                MessageBox.Show("Please select currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Set focus on Combobox
+                cmbFromCurrency.Focus();
+                return;
+            }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
