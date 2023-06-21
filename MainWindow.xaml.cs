f69b2d5fffa1a9fd108965186237efff8517714c
Valentin <valentin.chivliev02@gmail.com>
2023-06-22 00:57:41 +0300

    Input Validation and Completing the Logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,7 +27,6 @@ namespace CurrencyConverter_Static
             InitializeComponent();
             BindCurrency();
         }
-
         private void BindCurrency()
         {
             DataTable dtCurrency = new DataTable();
@@ -67,25 +67,66 @@ namespace CurrencyConverter_Static
                 txtCurrency.Focus();
                 return;
             }
-            // If currency is not selected or selected default text
-            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0) ;
+            // If currency "FROM" is not selected or selected default text
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
             {
                 // Show message
-                MessageBox.Show("Please select currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Please select currency FROM", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
                 // Set focus on Combobox
                 cmbFromCurrency.Focus();
                 return;
             }
-        }
+            // If currency "TO" is not selected or selected default text
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                // Show message
+                MessageBox.Show("Please select currency TO", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Set focus on Combobox
+                cmbToCurrency.Focus();
+                return;
+            }
 
+            // If "FROM" and "TO" have same values
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                // Convert from string to double
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                // Show converted currency in the label
+                // N2 place 00 after dot(.)
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N2");
+            }
+            else
+            {
+                // "FROM" value multiply with value from TextBox and the total dividet with "TO" value
+                ConvertedValue = (double.Parse(txtCurrency.Text))
+                    / double.Parse(cmbFromCurrency.SelectedValue.ToString()) * double.Parse(cmbToCurrency.SelectedValue.ToString());
+                // Show converted currency in the label
+                // N2 place 00 after dot(.)
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N2");
+            }
+        }
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-
+            ClearControls();
         }
-
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+            {
+                cmbFromCurrency.SelectedIndex = 0;
+            }
+            if (cmbToCurrency.Items.Count > 0)
+            {
+                cmbToCurrency.SelectedIndex = 0;
+            }
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
+        }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
